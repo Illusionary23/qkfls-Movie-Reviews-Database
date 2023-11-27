be80e0343d2dec7ceb2d8bc32d4215a2070ec9e5
Monimala Ghosh <149581488+Illusionary23@users.noreply.github.com>
2023-11-27 12:59:49 +0100

    Update server.js
    massage changed

--- server.js
+++ server.js
@@ -172,5 +172,5 @@ app.use(express.static(__dirname + '/views'));
 
 let port = process.env.PORT || 8080
 app.listen(port, function () {
-  console.log("To view app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
